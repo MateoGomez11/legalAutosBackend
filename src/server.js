38d40f08cf5b76513c9c10689fecfc3ece6a6e06
JuanDaviid01 <127440006+JuanDaviid01@users.noreply.github.com>
2024-03-11 10:20:55 -0500

    creación de clases

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const connection = require('./database/connection');
+const connection = require('./DataBase/connection');
 const app = express();
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 1338;
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
