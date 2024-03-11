8af663fa27b5712f597f568cf7b208801263f90a
JuanDaviid01 <127440006+JuanDaviid01@users.noreply.github.com>
2024-03-11 13:56:58 -0500

    atributos iniciales vehiculo

--- src/dataBase/sync.js
+++ src/dataBase/sync.js
@@ -1,9 +1,6 @@
 const connection = require('.connection');
 //Model
 const vehicle = require('../Model/Vehicle');
-const inventory = require('../Model/Inventory');
-const buyer = require('../Model/Buyer');
-const seller = require('../Model/Seller');
 const publication = require('../Model/Publication');
 const membership = require('../Model/Membership');
 const person = require('../Model/Person');
