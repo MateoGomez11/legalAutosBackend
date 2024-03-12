35945712b1175f127836c652450f1f52cad2c2d1
AndresRojas1704 <andrescamilropa@gmail.com>
2024-03-11 22:52:53 -0500

    Clase publicacion y controller publicacion

--- src/dataBase/sync.js
+++ src/dataBase/sync.js
@@ -5,11 +5,9 @@ const publication = require('../Model/Publication');
 const membership = require('../Model/Membership');
 const person = require('../Model/Person');
 
-const restaurant = require('../Models/restaurant');
-const product = require('../Models/product');
-const department = require('../Models/department');
-const city = require('../Models/city');
 
 //JSON
 const departamentjson = require('./jsonfiles/departmentjson');
 const cityjson = require('./jsonfiles/cityjson');
+
+
